42c80f750327b76541cacf57d24f52dc4165b198
Stephen Meycelino Fransisco <stephenmeycelinowork88@gmail.com>
2024-06-17 11:24:22 +0700

    feat(ui): edit section

--- app/(instructor)/instructor/courses/page.tsx
+++ app/(instructor)/instructor/courses/page.tsx
@@ -1,20 +1,38 @@
 import { Button } from "@/components/ui/button";
+import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs/server";
 import Link from "next/link";
 import { redirect } from "next/navigation";
 
-export default function CoursesPage() {
+export default async function CoursesPage() {
   const { userId } = auth();
 
   if (!userId) {
     return redirect("/sign-in");
   }
 
+  const courses = await db.course.findMany({
+    where: {
+      instructorId: userId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+
   return (
     <div className="px-6 py-4">
       <Link href={"/instructor/create-courses"}>
         <Button>Create New Courses</Button>
       </Link>
+      <div className="mt-10">
+        {courses.map((course) => (
+          <Link href={`/instructor/courses/${course.id}/basic`} key={course.id}>
+            {course.title}
+          </Link>
+        ))}
+      </div>
     </div>
   );
 }
---
+++ components/courses/EditCoursesForm.tsx
@@ -0,0 +1,272 @@
+"use client";
+
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Course } from "@prisma/client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import RichEditor from "../custom/RichEditor";
+import { ComboBox } from "../custom/ComboBox";
+import UploadFile from "../custom/UploadFile";
+import Link from "next/link";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { usePathname, useRouter } from "next/navigation";
+import { Trash } from "lucide-react";
+
+const formSchema = z.object({
+  title: z.string().min(2, {
+    message: "Title is required and minimum 2 characters",
+  }),
+  subtitle: z.string().optional(),
+  description: z.string().optional(),
+  categoryId: z.string().min(1, {
+    message: "Category is required",
+  }),
+  subCategoryId: z.string().min(1, {
+    message: "SubCategory is required",
+  }),
+  levelId: z.string().optional(),
+  imageUrl: z.string().optional(),
+  price: z.coerce.number().optional(),
+});
+
+interface EditCoursesFormProps {
+  course: Course;
+  categories: {
+    label: string; // name of category
+    value: string; // categoryId
+    subCategories: { label: string; value: string }[];
+  }[];
+  levels: { label: string; value: string }[];
+}
+
+export default function EditCoursesForm({
+  course,
+  categories,
+  levels,
+}: EditCoursesFormProps) {
+  const router = useRouter();
+  const pathName = usePathname();
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: course.title,
+      subtitle: course.subtitle || "",
+      description: course.description || "",
+      categoryId: course.categoryId,
+      subCategoryId: course.subCategoryId,
+      levelId: course.levelId || "",
+      imageUrl: course.imageUrl || "",
+      price: course.price || undefined,
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      await axios.patch(`/api/courses/${course.id}`, values);
+      toast.success("Course updated successfully");
+      router.refresh();
+    } catch (error) {
+      console.log("Failed to Update New Courses", error);
+      toast.error("Failed to Update New Courses");
+    }
+  };
+
+  const routes = [
+    {
+      label: "Basic Information",
+      path: `/instructor/courses/${course.id}/basic`,
+    },
+    {
+      label: "Course Content",
+      path: `/instructor/courses/${course.id}/section`,
+    },
+  ];
+
+  return (
+    <>
+      <div className="flex flex-col sm:flex-row gap-2 sm:justify-between mb-7">
+        <div className="flex gap-5">
+          {routes.map((route) => (
+            <Link href={route.path} key={route.path} className="flex gap-4">
+              <Button variant={pathName === route.path ? "default" : "ghost"}>
+                {route.label}
+              </Button>
+            </Link>
+          ))}
+        </div>
+
+        <div className="flex gap-4 items-start">
+          <Button variant="outline">Publish</Button>
+          <Button variant="default">
+            <Trash className="w-4 h-4" />
+          </Button>
+        </div>
+      </div>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="title"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Title</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="Ex:Web Development for Beginners"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="subtitle"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Subtitle</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="Ex: Become a Full-Stack with Just One Course"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="description"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Description</FormLabel>
+                <FormControl>
+                  <RichEditor
+                    placeholder="What is this course about?"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <div className="flex flex-wrap gap-10">
+            <FormField
+              control={form.control}
+              name="categoryId"
+              render={({ field }) => (
+                <FormItem className="flex flex-col">
+                  <FormLabel>Category</FormLabel>
+                  <FormControl>
+                    <ComboBox options={categories} {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="subCategoryId"
+              render={({ field }) => (
+                <FormItem className="flex flex-col">
+                  <FormLabel>SubCategory</FormLabel>
+                  <FormControl>
+                    <ComboBox
+                      options={
+                        categories.find(
+                          (category) =>
+                            category.value === form.watch("categoryId")
+                        )?.subCategories || []
+                      }
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="levelId"
+              render={({ field }) => (
+                <FormItem className="flex flex-col">
+                  <FormLabel>Level</FormLabel>
+                  <FormControl>
+                    <ComboBox options={levels} {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+
+          <FormField
+            control={form.control}
+            name="imageUrl"
+            render={({ field }) => (
+              <FormItem className="flex flex-col">
+                <FormLabel>Course Banner</FormLabel>
+                <FormControl>
+                  <UploadFile
+                    value={field.value || ""}
+                    onChange={(url) => field.onChange(url)}
+                    endpoint="courseBanner"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="price"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Price</FormLabel>
+                <FormControl>
+                  <Input
+                    type="number"
+                    step="0.01"
+                    placeholder="99.99"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <div className="flex gap-5">
+            <Link href={"/instructor/courses"}>
+              <Button variant="outline" type="button">
+                Cancel
+              </Button>
+            </Link>
+            <Button type="submit">Save</Button>
+          </div>
+        </form>
+      </Form>
+    </>
+  );
+}
